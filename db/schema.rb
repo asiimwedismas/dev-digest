81db28081851dad5331789ea8d25d743c1e6933c
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-09-22 19:02:19 +0300

    Scaffold and migrate user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,43 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_150257) do
+ActiveRecord::Schema.define(version: 2020_09_22_155841) do
 
-  create_table "articles", force: :cascade do |t|
-    t.integer "author_id", null: false
-    t.string "title"
-    t.text "text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_articles_on_author_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.integer "priority"
-    t.integer "article_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["article_id"], name: "index_categories_on_article_id"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.text "name"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "votes", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "article_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["article_id"], name: "index_votes_on_article_id"
-    t.index ["user_id"], name: "index_votes_on_user_id"
-  end
-
-  add_foreign_key "articles", "authors"
-  add_foreign_key "categories", "articles"
-  add_foreign_key "votes", "articles"
-  add_foreign_key "votes", "users"
 end
