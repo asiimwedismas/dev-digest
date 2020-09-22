51e2fe55906112bc9d3c00a23e2469a20ee84762
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-09-22 20:13:23 +0300

    Add user login and sign up actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,23 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
   # GET /users
-
+  # user selects name from list to login
   def index
     @users = User.all
   end
 
   # GET /users/1
+  # show articles created by the user
   def show; end
 
   # GET /users/new
+  # show create or sign up a new user
   def new
     @user = User.new
   end
 
   # POST /users
-
+  # save new user
   def create
     @user = User.new(user_params)
 
@@ -26,6 +28,26 @@ class UsersController < ApplicationController
       else
         format.html { render :new }
       end
+    end
+  end
+
+  # logout current user
+  def logout
+    session.destroy
+    redirect_to root_path
+  end
+
+  def authenticate_user
+    @user = User.find_by(name: params[:name])
+
+    if @user.nil?
+      flash[:notice] = 'This name is not registered'
+      redirect_to login_path
+    else
+      session[:current_user_id] = @user.id
+      session[:current_username] = @user.name
+      flash[:notice] = 'User Logged in'
+      redirect_to user_path(@user)
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   resources :votes
   resources :categories
   resources :articles
-  resources :users
+  resources :users, only: %i[show]
+
+  get 'login' => 'users#index'
+  post 'login' => 'users#authenticate_user'
+  get 'logout' => 'users#logout'
+  get 'signup' => 'users#new'
+  post 'signup' => 'user#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
