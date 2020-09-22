e5cf8d94cfad7aac7286bb1339f680755a28ab01
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-09-22 17:58:07 +0300

    Scaffold article

---
+++ db/migrate/20200922145753_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.references :author, null: false, foreign_key: true
+      t.string :title
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
