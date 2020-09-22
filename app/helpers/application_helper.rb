2173f3a2b4b43aeb6dcffa753f24c1e9714cb2dd
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-09-22 20:12:41 +0300

    Add application helpers to find logged in user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_loggedin?
+    !session[:current_user_id].nil?
+  end
+
+  def current_user
+    User.find(session[:current_user_id]) if user_loggedin?
+  end
 end
