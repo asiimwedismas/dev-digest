e671d4f22ccf1019b78f9ddfe22a06388f34c23a
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-10-01 18:30:52 +0300

    Add helper to display the vote or unvote button

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,10 @@
 module ArticlesHelper
+  def vote_or_unvote_btn(article)
+    vote = Vote.find_by(article: article, user: current_user)
+    if vote
+      link_to('Un-vote!', article_vote_path(id: vote.id, article_id: article.id), method: :delete, class: 'btn')
+    else
+      link_to('Vote!', article_votes_path(article_id: article.id), method: :post, class: 'btn')
+    end
+  end
 end
