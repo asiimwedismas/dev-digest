b914b03626160ea00c692e8132c9a88f84f7b084
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-10-01 18:29:53 +0300

    Add assocaitions for votes and image data

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,9 @@ class Article < ApplicationRecord
   belongs_to :author, foreign_key: 'author_id', class_name: 'User'
   has_many :article_categories
   has_many :categories, through: :article_categories
+
+  has_many :votes, dependent: :destroy
+
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
 end
