920ad9d0ab205127e63239c6d76c25e3f3f163fc
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-10-01 18:29:33 +0300

    Add validation for name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 20 }
+
   has_many :articles, foreign_key: :author_id, dependent: :destroy
   has_many :votes, dependent: :destroy
 end
