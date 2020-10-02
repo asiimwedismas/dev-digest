6ca3b48119114f21975e7a9c4411e59e7425e2c5
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-10-02 19:03:14 +0300

    Change from paperlcip to store image url instead sice heroku does not support images

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,6 @@ class Article < ApplicationRecord
 
   has_many :votes, dependent: :destroy
 
-  has_attached_file :image
-  validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
+  # has_attached_file :image
+  # validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_152353) do
+ActiveRecord::Schema.define(version: 2020_10_02_145513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_09_30_152353) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.text "image"
     t.index ["author_id"], name: "index_articles_on_author_id"
   end
 
