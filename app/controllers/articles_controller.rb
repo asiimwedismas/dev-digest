f58e8e3aaa4155441271db144e8c0a6cc085b8ed
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-10-02 23:26:12 +0300

    Add edit article feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,23 @@
 class ArticlesController < ApplicationController
   include ApplicationHelper
-  before_action :authenticate, only: %i[new create]
+  before_action :set_article, only: %i[show edit update]
+  before_action :authenticate, only: %i[new edit update create]
 
-  def show
-    @article = Article.find(params[:id])
+  def show; end
+
+  def edit
+    @categories = Category.all
+  end
+
+  def update
+    respond_to do |format|
+      if @article.update(article_params)
+        @article.article_categories.update(category_id: params[:category_id])
+        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def new
@@ -16,8 +30,7 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.save
-        selected_category = Category.find(params[:category_id])
-        @article.article_categories.create(category: selected_category)
+        @article.article_categories.create(category_id: params[:category_id])
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
       else
         format.html { render :new }
@@ -26,6 +39,10 @@ class ArticlesController < ApplicationController
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :text, :image)
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,9 +4,15 @@ module ArticlesHelper
 
     vote = Vote.find_by(article: article, user: current_user)
     if vote
-      link_to('Un-vote!', article_vote_path(id: vote.id, article_id: article.id), method: :delete, class: 'btn')
+      link_to('Un-vote!', article_vote_path(id: vote.id, article_id: article.id), method: :delete, class: 'btn btn-danger')
     else
-      link_to('Vote!', article_votes_path(article_id: article.id), method: :post, class: 'btn')
+      link_to('Vote!', article_votes_path(article_id: article.id), method: :post, class: 'btn btn-success')
     end
+  end
+
+  def link_to_edit_article(article)
+    return if session[:current_user_id] != article.author.id
+
+    link_to('Edit Article', edit_article_path(article), class: 'btn btn-link')
   end
 end
