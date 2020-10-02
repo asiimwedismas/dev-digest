a0225c9bf58b49154a6f4674a6dc762d812304ef
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-10-02 17:02:38 +0300

    Add dependdant destroy

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :author, foreign_key: 'author_id', class_name: 'User'
   has_many :article_categories
-  has_many :categories, through: :article_categories
+  has_many :categories, through: :article_categories, dependent: :destroy
 
   has_many :votes, dependent: :destroy
 
