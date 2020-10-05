22fc25d4c893c39131a6cc728c08bfe63f8435e0
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-10-05 21:41:51 +0300

    add: validattions for article inputs from user

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,13 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 1000,
+                                              too_long: '256 maximum allowed characters in an article title.' }
+
+  validates :text, presence: true
+  validates :image, presence: true
+
   belongs_to :author, foreign_key: 'author_id', class_name: 'User'
   has_many :article_categories
   has_many :categories, through: :article_categories, dependent: :destroy
 
   has_many :votes, dependent: :destroy
-
-  # has_attached_file :image
-  # validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
 end
