d57e75d45c01c9a51de65ef8bdd82cf55b0e46bb
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-10-06 22:43:17 +0300

    fix linter errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,11 +23,13 @@ module ApplicationHelper
     links_string = ''
 
     if !user_loggedin?
-      links_string = links_string + link_to('LOGIN', login_path) + ' | ' + link_to('REGISTER', signup_path)
+      links_string += link_to('LOGIN', login_path)
+      links_string = links_string + ' | ' + link_to('REGISTER', signup_path)
     else
       icon = '<i class="material-icons">note_add</i>'.html_safe
       links_string += link_to(icon, new_article_path, class: 'orange_link')
-      links_string = links_string + link_to(current_user.name, user_path(current_user)) + ' | ' + link_to('log out', logout_path)
+      links_string += link_to(current_user.name, user_path(current_user))
+      links_string = links_string + ' | ' + link_to('log out', logout_path)
     end
     links_string.html_safe
   end
