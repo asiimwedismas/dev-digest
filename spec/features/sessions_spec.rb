1bc6013a387f1b9683ea266a7dff47046799d74a
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-10-06 20:41:05 +0300

    add: test for logout feature

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -2,11 +2,19 @@ require 'rails_helper'
 
 RSpec.feature 'Sessions', type: :feature do
   describe 'Authentication' do
-    it 'log in should succeed' do
+    before :each do
       User.new(name: 'dismas').save
       visit login_path
       click_link 'login'
+    end
+
+    it 'log in should succeed' do
       expect(page).to have_content 'User Logged in'
+    end
+
+    it 'log out should succeed' do
+      click_link 'log out'
+      expect(page).to have_content 'Successfully logged out'
     end
   end
 end
