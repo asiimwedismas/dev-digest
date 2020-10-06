f009c6e01b76307530baed94ca216d441eb49937
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-10-06 20:15:55 +0300

    add: validation tests for article attributes

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
+  context 'validations' do
+    before :each do
+      @author = User.new(name: 'dismas')
+      @author.save
+    end
+
+    it 'title must be present' do
+      article = Article.new(author: @author,
+                            title: '',
+                            text: 'article text',
+                            image: 'www.image.com').save
+      expect(article).to eq(false)
+    end
+
+    it 'content must be present' do
+      article = Article.new(author: @author,
+                            title: 'my title',
+                            text: '',
+                            image: 'www.image.com').save
+      expect(article).to eq(false)
+    end
+
+    it 'image url must be present' do
+      article = Article.new(author: @author,
+                            title: 'my title',
+                            text: 'content',
+                            image: '').save
+      expect(article).to eq(false)
+    end
+  end
+
   context 'associations' do
     it 'should have many categories' do
       article = Article.reflect_on_association(:categories)
