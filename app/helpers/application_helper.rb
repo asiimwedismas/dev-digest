82ad6959c30f0e51312a060394c18a473ba26521
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-10-06 22:18:31 +0300

    remove: logic from our application layout view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,17 @@ module ApplicationHelper
   def authenticate
     redirect_to login_path if session[:current_user_id].nil?
   end
+
+  def login_links
+    links_string = ''
+
+    if !user_loggedin?
+      links_string = links_string + link_to('LOGIN', login_path) + ' | ' + link_to('REGISTER', signup_path)
+    else
+      icon = '<i class="material-icons">note_add</i>'.html_safe
+      links_string += link_to(icon, new_article_path, class: 'orange_link')
+      links_string = links_string + link_to(current_user.name, user_path(current_user)) + ' | ' + link_to('log out', logout_path)
+    end
+    links_string.html_safe
+  end
 end
