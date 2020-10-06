375aa250ac54e6f916e95b78afb50d4432b16737
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-10-06 21:42:16 +0300

    add: special future

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    user = User.find(params[:id])
+    @articles = user.articles.includes(:categories)
   end
 
   def create
